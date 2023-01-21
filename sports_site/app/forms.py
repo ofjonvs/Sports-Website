f08808d063e56f5384102b7f4476088b6b45ba13
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2023-01-21 15:30:06 -0500

    Add files via upload

---
+++ sports_site/app/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'title_tag', 'author', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),            
+        }
+
+class EditArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'title_tag', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),            
+        }
