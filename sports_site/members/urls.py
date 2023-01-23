ec9fca33f38fe2bd9022b5bf75d71a11b213e62b
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2023-01-23 00:38:58 -0500

    Delete sports_site directory

--- sports_site/members/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import UserRegisterView
-
-urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register'),
-]
