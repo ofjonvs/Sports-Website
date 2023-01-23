32b2242a1ac8d425ab0632074e3b1d3beecf737f
Jonas da Silva <jonas.fjds@gmail.com>
2023-01-23 00:18:50 -0500

    update

---
+++ sports_site/app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+# from . import views
+from .views import HomeView, ArticleDetailView, DeleteArticleView, AddArticleView, UpdateArticleView
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article'),
+    path('new-article/', AddArticleView.as_view(), name='new-article'),
+    path('article/edit/<int:pk>', UpdateArticleView.as_view(), name='update-article'),
+    path('article/delete/<int:pk>', DeleteArticleView.as_view(), name='delete-article'),
+]
---
+++ sports_site/app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Article
+from .forms import ArticleForm, EditArticleForm
+from django.urls import reverse_lazy
+
+# def home(request):
+#     return render(request, 'home.html', {})
+
+
+class HomeView(ListView):
+    model = Article
+    template_name = 'home.html'
+    ordering = ['-article_date']
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles.html'
+
+class AddArticleView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'add_article.html'
+    # fields = '__all__'
+    # fields = ('title', 'body')
+
+class UpdateArticleView(UpdateView):
+    model = Article
+    template_name = 'edit_article.html'
+    # fields = ['title', 'title_tag', 'body']
+    form_class = EditArticleForm
+
+class DeleteArticleView(DeleteView):
+    model = Article
+    template_name = 'delete_article.html'
+    success_url = reverse_lazy('home')
